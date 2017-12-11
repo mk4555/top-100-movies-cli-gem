a828daaef63b527a9bbf78096efb60cdfb8cb909
Min Suk <Kim@Mins-MBP.fios-router.home>
2017-12-11 17:56:35 -0500

    added rank to movie class attributes

--- lib/top_100_movies/movie.rb
+++ lib/top_100_movies/movie.rb
@@ -1,10 +1,11 @@
 class Top100Movies::Movie
-  attr_accessor :name, :url, :description, :rating, :genre, :directors,
+  attr_accessor :rank, :name, :url, :description, :rating, :genre, :directors,
   :writers, :debut, :box_office, :length, :studio
 
   @@all = []
 
-  def initialize(name=nil, url=nil)
+  def initialize(rank=nil, name=nil, url=nil)
+    @rank = rank
     @name = name
     @url = url
     @@all << self
