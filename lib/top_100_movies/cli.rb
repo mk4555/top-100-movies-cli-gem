30aa1f3ea05567f151383413461b9c1f828c9476
Min Suk <Kim@Mins-MBP.fios-router.home>
2017-12-11 18:17:07 -0500

    added mock scraping methods and mock cli setup

--- lib/top_100_movies/cli.rb
+++ lib/top_100_movies/cli.rb
@@ -1,7 +1,10 @@
 class Top100Movies::CLI
-  def call
+  def self.call
     puts "Hellow welcome to top 100 movies!"
     puts "Data from Rotten Tomatoes"
-    
+    # doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+    # binding.pry
+    Top100Movies::Scraper.scrape_movies
+
   end
 end
--- lib/top_100_movies/scraper.rb
+++ lib/top_100_movies/scraper.rb
@@ -1,3 +1,21 @@
 class Top100Movies::Scraper
-   
+
+  def self.get_page
+    Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+  end
+
+  def self.scrape_movies
+    table = self.get_page.at('.table')
+    cells = []
+    table.search('tr').each do |tr|
+      cells << tr.search('th, td')
+      # puts "#{tr.search('th, td')}"
+    end
+
+    cells.each do |cell|
+      puts "#{cell.search(".tMeterScore").text.strip}" # Extracts RT scores
+      puts "#{cell.search(".unstyled").text.strip}"
+    end
+  end
+
 end
