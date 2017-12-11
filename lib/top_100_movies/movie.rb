f7d8738336d2ac7a234eb76c38fd2dffa8f620d8
Min Suk <Kim@Mins-MBP.fios-router.home>
2017-12-11 17:49:24 -0500

    Movie class variables and initial method added

---
+++ lib/top_100_movies/movie.rb
@@ -0,0 +1,15 @@
+class Top100Movies::Movie
+  attr_accessor :name, :url, :description, :rating, :genre, :directors,
+  :writers, :debut, :box_office, :length, :studio
+
+  @@all = []
+
+  def initialize(name=nil, url=nil)
+    @name = name
+    @url = url
+    @@all << self
+  end
+
+  
+
+end
