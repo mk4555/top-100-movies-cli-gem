c9d9ab08817545df4d4658727e86018f22580112
Min Suk <Kim@Mins-MBP.fios-router.home>
2017-12-11 18:30:28 -0500

    scrape_movies can successfully scrape data

--- lib/top_100_movies/scraper.rb
+++ lib/top_100_movies/scraper.rb
@@ -12,9 +12,20 @@ class Top100Movies::Scraper
       # puts "#{tr.search('th, td')}"
     end
 
+    # <td class="bold">100.</td><td>
+    #     <span class="tMeterIcon tiny">
+    #                 <span class="icon tiny certified_fresh"></span>
+    #                 <span class="tMeterScore"> 100%</span>
+    #             </span>
+    #         </td><td>
+    #     <a href="/m/grapes_of_wrath" class="unstyled articleLink">
+    #         The Grapes of Wrath (1940)</a>
+    # </td><td class="right hidden-xs">43</td>
+
     cells.each do |cell|
       puts "#{cell.search(".tMeterScore").text.strip}" # Extracts RT scores
-      puts "#{cell.search(".unstyled").text.strip}"
+      puts "#{cell.search(".unstyled").text.strip}" # Extracts Movie Name
+      cell.search("a").each {|link| puts "#{link['href']}"} # Extracts URL
     end
   end
 
