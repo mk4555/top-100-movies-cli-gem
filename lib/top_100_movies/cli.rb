3b85d49eda6c06711503441c3263dd4bc9135484
Min Suk <Kim@Mins-MBP.fios-router.home>
2017-12-12 00:50:16 -0500

    movie detail scraper and printer added

--- lib/top_100_movies/cli.rb
+++ lib/top_100_movies/cli.rb
@@ -5,18 +5,24 @@ class Top100Movies::CLI
     puts "******        WELCOME TO TOP 100 MOVIES!        ******"
     puts "******************************************************"
     puts ""
-    # start
-    doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/m/the_wizard_of_oz_1939"))
+    start
+    # doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/m/the_wizard_of_oz_1939"))
     # binding.pry
 
     # doc.search("div#movieSynopsis").first.text.strip -> Synopsis
     # doc.search(".meta-value a[href]").first.text.strip -> Genres
     # arr = doc.search(".meta-value").map{|i| i.text.strip} -> list of all
 
-    arr = doc.search(".meta-value").map{|i| i.text.strip}
-    arr.delete(arr[1]) # delete genres
-    binding.pry
-    #
+    # arr = doc.search(".meta-value").map{|i| i.text.strip}
+    # arr.delete(arr[1]) # delete genres
+    # movie = Top100Movies::Movie.find(1)
+    # movie.rating = arr[0]
+    # movie.directors = arr[1]
+    # movie.writers = arr[2]
+    # movie.release_date = arr[3]
+    # movie.box_office = arr[5]
+    # movie.runtime = arr[6]
+    # binding.pry
   end
 
   def start
@@ -26,8 +32,9 @@ class Top100Movies::CLI
     input = gets.chomp
     if input.to_i > 0 && input.to_i <= 100
       puts "correct input"
-      # movie = Top100Movies::Movie.find(input.to_i)
-      # Top100Movies::Movie.print_movie(movie)
+      movie = Top100Movies::Movie.find(input.to_i)
+      Top100Movies::Movie.print_movie(movie)
+      # movie.print_movie
     elsif input == "exit"
       puts "See you later!"
       exit
--- lib/top_100_movies/movie.rb
+++ lib/top_100_movies/movie.rb
@@ -34,10 +34,36 @@ class Top100Movies::Movie
   end
 
   def self.find(rank)
+    self.scrape_details(@@all[rank-1])
     @@all[rank-1]
   end
 
-  def self.print(movie)
-
+  def self.print_movie(movie)
+    puts ""
+    puts "#{movie.rank}. #{movie.name}"
+    puts ""
+    puts "SYNOPSIS"
+    puts "#{movie.synopsis}"
+    puts ""
+    puts "Directors:\t#{movie.directors}"
+    puts "Writers:\t#{movie.writers}"
+    puts "Release Date:\t#{movie.release_date}"
+    puts "Box Office:\t#{movie.box_office}"
+    puts ""
   end
+
+  def self.scrape_details(movie)
+    doc = Nokogiri::HTML(open("#{movie.url}"))
+    movie.synopsis = doc.search("div#movieSynopsis").first.text.strip
+    arr = doc.search(".meta-value").map{|i| i.text.strip}
+    arr.delete(arr[1]) # delete genres
+    movie.rating = arr[0]
+    movie.directors = arr[1]
+    movie.writers = arr[2]
+    movie.release_date = arr[3]
+    movie.box_office = arr[5]
+    movie.runtime = arr[6]
+  end
+
+
 end
