db90c1cc1b87d1bd4211700d651c6663d870d16a
Min Suk <Kim@Mins-MBP.fios-router.home>
2017-12-11 20:07:32 -0500

    added mock cli for inputs

--- lib/top_100_movies/cli.rb
+++ lib/top_100_movies/cli.rb
@@ -1,19 +1,28 @@
 class Top100Movies::CLI
   def call
     Top100Movies::Scraper.new.generate_movies
-    puts "Hello welcome to top 100 movies!"
-    puts "Data from Rotten Tomatoes"
-    # doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
-    # binding.pry
+    puts "******************************************************"
+    puts "******        WELCOME TO TOP 100 MOVIES!        ******"
+    puts "******************************************************"
+    puts ""
     start
   end
 
   def start
     list_movies
+    puts ""
+    print "Please enter the number of the movie you'd like to know more about: "
+    input = gets.chomp
+    if input.to_i > 0 && input.to_i <= 100
+      puts "correct input"
+    elsif input == "exit"
+      puts "See you later!"
+      exit
+    end
   end
 
   def list_movies
-    puts "| Rank |      Name      | Rating |"
+    puts " Rank |      Name      | Rating "
     Top100Movies::Movie.all.each do |movie|
       puts " #{movie.rank}. #{movie.name}  #{movie.rating}"
     end
